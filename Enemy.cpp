743e79a40cd1a1ac03bccd114b8e90a34ee70344
jozycortez <josecortez19590@student.hartnell.edu>
2015-05-08 13:22:34 -0700

    Enemy Class header and implementation
    This is the enemy base class.

---
+++ Enemy.cpp
@@ -0,0 +1,46 @@
+
+#include "Enemy.h"
+#include <cassert>
+Enemy::Enemy()
+{
+    positionX = 0;
+    positionY = 0;
+    icon = '*';
+    award = 100;
+    enemy_health = 1;
+    is_dead = false;
+}
+void Enemy::setX(int x)
+{
+    positionX = x;
+}
+void Enemy::setY(int y)
+{
+    positionY = y;
+}
+void Enemy::setIcon(char icon)
+{
+    this -> icon = icon;
+}
+void Enemy::setAward(int award)
+{
+    this -> award = award;
+}
+void Enemy::setEnemyHealth(int enemy_health)
+{
+    this -> enemy_health = enemy_health;
+}
+void Enemy::setIsDead(bool is_dead)
+{
+    this -> is_dead = is_dead;
+}
+void Enemy::isHit()
+{
+    assert(enemy_health > 0);
+    enemy_health--;
+}
+void Enemy::checkIsDead()
+{
+    if (enemy_health == 0)
+        is_dead = true;
+}
---
+++ Enemy.h
@@ -0,0 +1,35 @@
+#ifndef ENEMY_H_INCLUDED
+#define ENEMY_H_INCLUDED
+
+
+
+
+class Enemy
+{
+    private:
+        int enemy_health;
+        char icon;
+        int award;
+        int positionY;
+        int positionX;
+        bool is_dead;
+    public:
+        Enemy();
+        int getX()const {return positionX;}
+        int getY()const {return positionY;}
+        int getAward() const {return award;}
+        int getEnemyHealth()const {return enemy_health;}
+        char getIcon()const {return icon;}
+        bool getIsDead()const {return is_dead;}
+        void setX(int x);
+        void setY(int y);
+        void setIcon(char icon);
+        void setAward(int award);
+        void setEnemyhealth(int enemy_health);
+        void setIsDead(bool is_dead);
+        void isHit();
+        void checkIsDead();
+
+};
+
+#endif // ENEMY_H_INCLUDED
