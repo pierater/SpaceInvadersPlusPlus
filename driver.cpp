4dccb787167c1873645c4e4efa8e1d6d7a56d071
EddySigma <juanegarcia19258@student.hartnell.edu>
2015-05-08 15:07:58 -0700

    driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,21 @@
+// *****************
+// driver.cpp
+// *****************
+
+#include "Enemy.h"
+#include "Ship.h"
+#include "Screen.h"
+#include "Base.h"
+#include "Lazer.h"
+
+#include <iostream>
+#include <ncurses.h>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
